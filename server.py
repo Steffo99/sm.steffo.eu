fe215583808013f065981d9f7dd64114c40e89d6
Stefano Pigozzi <ste.pigozzi@gmail.com>
2016-12-21 18:48:36 +0100

    First working commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from sm import steammatch
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html.j2")
+
+@app.route("/list", methods=["POST"])
+def listpage():
+    if request.form["input"] == "":
+        return "error: no input"
+    if request.form["op"] == "":
+        return "error: no op"
+    raw_users = request.form["input"].split(",")
+    users = list()
+    for user in raw_users:
+        users.append(user.strip(" ").replace("https://steamcommunity.com/id/", ""))
+    if len(users) < 2:
+        return "error: not enough users"
+    if request.form["op"] == "and":
+        return render_template("list.html.j2", l=steammatch.and_games(users))
+    elif request.form["op"] == "or":
+        return render_template("list.html.j2", l=steammatch.or_games(users))
+    elif request.form["op"] == "xor":
+        if len(users) > 2:
+            return "error: too many users"
+        return render_template("list.html.j2", l=steammatch.xor_games(users))
