bd9faa388e670877d546636f7eb3a5a460bbdf68
Stefano Pigozzi <ste.pigozzi@gmail.com>
2017-01-03 15:49:40 +0100

    Edit main.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from smweb.sm import steammatch
 app = Flask(__name__)
 
@@ -8,25 +8,23 @@ def home():
 
 @app.route("/list", methods=["POST"])
 def listpage():
-    if request.form["input"] == "":
-        return "error: no input"
     if request.form["op"] == "":
-        return "error: no op"
+        flash('ERROR: No operation selected.')
+        return redirect(url_for('/'))
     raw_users = request.form["input"].split(",")
     users = list()
     for user in raw_users:
-        users.append(user.strip(" ").replace("https://steamcommunity.com/id/", ""))
+        user = user.strip(" ").replace("https://steamcommunity.com/id/", "").replace("https://steamcommunity.com/profile/", "").replace("http://steamcommunity.com/id/", "").replace("http://steamcommunity.com/profile/", "")
+        if user not in users:
+            users.append()
     if len(users) < 2:
-        return "error: not enough users"
+        flash('ERROR: Not enough profiles specified.')
+        return redirect(url_for('/'))
     if request.form["op"] == "and":
         return render_template("list.html.j2", l=steammatch.and_games(users))
     elif request.form["op"] == "or":
         return render_template("list.html.j2", l=steammatch.or_games(users))
     elif request.form["op"] == "xor":
-        if len(users) > 2:
-            return "error: too many users"
         return render_template("list.html.j2", l=steammatch.xor_games(users[0], users[1]))
     elif request.form["op"] == "diff":
-        if len(users) > 2:
-            return "error: too many users"
         return render_template("list.html.j2", l=steammatch.diff_games(users[0], users[1]))
