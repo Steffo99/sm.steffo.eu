3cdfbd4c669173fa90b23099da6be39cc2df793a
Stefano Pigozzi <ste.pigozzi@gmail.com>
2017-01-04 15:26:38 +0100

    Fixed #1 and #2, new error page

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from smweb.sm import steammatch
+from sm import steammatch
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,18 +8,22 @@ def home():
 
 @app.route("/list", methods=["POST"])
 def listpage():
-    if request.form["op"] == "":
-        flash('ERROR: No operation selected.')
-        return redirect(url_for('/'))
-    raw_users = request.form["input"].split(",")
+    try:
+        if request.form["op"] == "":
+            return render_template("error.html.j2", errortitle="Error", errordesc="No operation specified.")
+    except KeyError:
+        return render_template("error.html.j2", errortitle="Error", errordesc="No operation specified.")
+    try:
+        raw_users = request.form["input"].split(",")
+    except KeyError:
+        raw_users = []
     users = list()
     for user in raw_users:
         user = user.strip(" ").replace("https://steamcommunity.com/id/", "").replace("https://steamcommunity.com/profile/", "").replace("http://steamcommunity.com/id/", "").replace("http://steamcommunity.com/profile/", "")
         if user not in users:
             users.append(user)
     if len(users) < 2:
-        flash('ERROR: Not enough profiles specified.')
-        return redirect(url_for('/'))
+        return render_template("error.html.j2", errortitle="Error", errordesc="Not enough profiles specified.")
     if request.form["op"] == "and":
         return render_template("list.html.j2", l=steammatch.and_games(users))
     elif request.form["op"] == "or":
